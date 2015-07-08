eb76984bc5818bec459c011963f4ad678d35d1a8
marksteinbrick <marksteinbrick@collage.harvard.edu>
2015-07-08 09:18:40 +0200

    Added another column to Answer Model

--- application/models/Answer_model.php
+++ application/models/Answer_model.php
@@ -5,6 +5,7 @@ class Answer_model extends CI_Model {
     public $question_id;
     public $vote;
     public $answer_id;
+    public $answer_letter;
 
     function __construct()
     {
@@ -15,8 +16,10 @@ class Answer_model extends CI_Model {
     function insert_entry()
     {
         $this->body   = $_POST['body']; // please read the below note
-        $this->question_id = $_POST['question'];
+        $this->question_id = $_POST['question_id'];
         $this->vote = $_POST['vote']
+        $this->answer_id = $_POST['answer_id']
+        $this->answer_letter = $_POST['answer_letter']
 
         $this->db->insert('entries', $this);
     }
