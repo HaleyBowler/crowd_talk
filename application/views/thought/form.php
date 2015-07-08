3e570fd7999d58d7e5269d470e7d45780cdea425
HaleyBowler <hjb2dq@virginia.edu>
2015-07-08 21:44:29 +0200

    Added styling

--- application/views/thought/form.php
+++ application/views/thought/form.php
@@ -5,8 +5,7 @@
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>" />
 </head>
-<body>
-	<div class="container center">
+<body class="thought-form">
 	<form action="insert_into_db" method="post">
 		<div class="form-group">
 		<label for="thought">Thought</label>
