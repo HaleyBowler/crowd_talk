97f23ee0a056e4aebd3bf5bc415dc79aed33271a
HaleyBowler <hjb2dq@virginia.edu>
2015-07-08 10:58:42 +0200

    HTML geolocation

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,27 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->library('googlemaps');
 
-		$this->load->view('home.php');
-	}
+		$config = array();
+		$config['center'] = 'auto';
+		$config['onboundschanged'] = 'if (!centreGot) {
+			var mapCentre = map.getCenter();
+			marker_0.setOptions({
+				position: new google.maps.LatLng(mapCentre.lat(), mapCentre.lng()) 
+			});
+}
+centreGot = true;';
+$this->googlemaps->initialize($config);
+
+// set up the marker ready for positioning 
+// once we know the users location
+$marker = array();
+$this->googlemaps->add_marker($marker);
+$data['map'] = $this->googlemaps->create_map();
+$lat = 'mapCentre.lat';
+
+$this->load->view('home.php', $lat);
+}
 }
 ?>
--- application/views/home.php
+++ application/views/home.php
@@ -3,6 +3,10 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>Home</title>
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+		<!--<script type="text/javascript">
+		var centreGot = false;
+	</script>
+	<?php echo $map['js']; ?>-->
 </head>
 <body>
 	<div class="page-header">
@@ -10,6 +14,25 @@
 	</div>
 	<a class="btn btn-default" href="thought/form" role="button">Thoughts?</a>
 	<a class="btn btn-default" href="question" role="button">Daily Question</a>
+	<p id="lat"></p>
+
+
+	<script>
+	    var x = document.getElementById("lat");
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+		} else {
+			alert('Error.');
+		}
+		function successFunction(position) {
+			var lat = position.coords.latitude;
+			var long = position.coords.longitude;
+			x.innerHTML = lat;
+		}
+	</script>
+
+	<!--<body><?php echo $map['html']; ?></body>-->
 	<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.11.3.min.js"); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </body>
