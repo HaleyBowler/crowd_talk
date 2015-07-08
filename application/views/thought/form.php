662a64e7ba1fbad416d8cadb690e75c840941991
HaleyBowler <hjb2dq@virginia.edu>
2015-07-08 21:26:40 +0200

    added style

--- application/views/thought/form.php
+++ application/views/thought/form.php
@@ -3,15 +3,18 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>Thoughts</title>
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>" />
 </head>
 <body>
+	<div class="container center">
 	<form action="insert_into_db" method="post">
 		<div class="form-group">
-		<label for="thought">Thought</label><br/>
-			<input type = 'text' name='body'>
+		<label for="thought">Thought</label>
+			<textarea class="form-control" name="body" type="text" rows="10" autofocus></textarea>
 		</div>
-		<button type="submit" class="btn btn-default">Submit</button>
+		<button type="submit" class="btn submit-thought" placeholder="Anonymously share you thoughts!">Submit</button>
 	</form>
+	</div>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.11.3.min.js"); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </body>
